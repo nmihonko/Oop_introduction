b350039343a269d7ab7aa3193b06070000ce813a
Nazar Mihonko <migonkonazar@gmail.com>
2026-04-14 16:31:49 +0300

    Add functional and fix mistakes

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    static void main(String[] args) {
+package app;
 
+// Виправте цей клас
+public class Main {
+
+    public static void main(String[] args) {
+
+        Customer Oleg = getCustomer(getData("Oleg", "+2983918312983"));
+        Customer Masha = getCustomer(getData("Masha", "+2983918312983"));
+        Customer Nazar = getCustomer(getData("Nazar", "+2983918312983"));
+        getOutput(Oleg);
+        getOutput(Masha);
+        getOutput(Nazar);
+    }
+
+    public static String[] getData(String name, String phone) {
+        return new String[]{name, phone};
+    }
+
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(Customer customer) {
+        System.out.println("Customer: " + customer.getName() +
+                ", phone " + customer.getPhone());
     }
 }
